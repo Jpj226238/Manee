3a1f9c02847821b557b3eaa203ca7b5fd5ba18d4
Jpj226238 <nicecrazy001@gmail.com>
2025-07-21 08:17:04 +0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,13 @@
+/* Place your JavaScript in this file */
+const userLang = navigator.language || navigator.userLanguage;
 
+if (userLang.startsWith("th")) {
+  // ภาษาไทย
+  window.location.href = "/th/index.html";
+} else if (userLang.startsWith("ja")) {
+  // ภาษาญี่ปุ่น
+  window.location.href = "/ja/index.html";
+} else {
+  // ค่า default เป็นภาษาอังกฤษ
+  window.location.href = "/en/index.html";
+}
